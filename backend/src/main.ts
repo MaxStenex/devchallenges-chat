5426dcc66a8841409ccf7d35a60d3d1c477caf9c
Maxim <yourstenex@gmail.com>
2022-08-09 19:35:27 +0300

    fix: enable cors on backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.enableCors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  });
 
   await app.listen(5000);
 }
