cbaa4ceaef1e1040e369c32097ec7b3f94a1609a
Maxim <yourstenex@gmail.com>
2022-08-20 15:35:43 +0300

    build: added clearMocks in jest config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -8,6 +8,7 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/src"],
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["./jest.setup.js"],
+  clearMocks: true,
 };
 
 module.exports = createJestConfig(customJestConfig);
