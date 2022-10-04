a11f66f3f5e736227b8d219ebfdecab135dde087
Maxim <yourstenex@gmail.com>
2022-10-04 22:41:50 +0300

    fix: removed channel gateway, made message model sender as optional field

--- backend/src/channel/channel.module.ts
+++ backend/src/channel/channel.module.ts
@@ -2,12 +2,11 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { UserModule } from "src/user/user.module";
 import { ChannelController } from "./channel.controller";
-import { ChannelGateway } from "./channel.gateway";
 import { ChannelService } from "./channel.service";
 
 @Module({
   imports: [PrismaModule, UserModule],
   controllers: [ChannelController],
-  providers: [ChannelService, ChannelGateway],
+  providers: [ChannelService],
 })
 export class ChannelModule {}
